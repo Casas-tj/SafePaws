51a4b0575bd3912b3a1a4121044e3791b8847254
Zuriñe <casastejadazurine@gmail.com>
2026-05-18 00:46:46 +0200

    core: decorators.py añadido

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def error_403(request, exception=None):
+    return render(request, '403.html', status=403)
--- apps/owners/views.py
+++ apps/owners/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from apps.core.decorators import permisos_requeridos, requiere_permiso
 from apps.owners.services.owners_services import OwnerService
 
 
-@login_required
+@permisos_requeridos('owners.view_owner')
 def owners(request):
 
     owners = OwnerService.list_owners()
@@ -14,6 +15,7 @@ def owners(request):
         'headersubtitle': 'Listado de propietarios y adoptantes',
         "btn_nuevo": "Nuevo Propietario",
         "form_url": 'owners_form',
+        "nuevo_perm": "owners.add_owner",
         'btn_back': 'Volver',
         'back_url': 'home',
     })
@@ -26,6 +28,11 @@ def owners_form_edit(request, owner_id=None):
     # 📋 GET → mostrar formulario
     # =========================
     if request.method == "GET":
+
+        if owner_id:
+            requiere_permiso(request, 'owners.change_owner')
+        else:
+            requiere_permiso(request, 'owners.add_owner')
 
         owner = None
         if owner_id:
@@ -43,6 +50,11 @@ def owners_form_edit(request, owner_id=None):
     # 💾 POST → guardar
     # =========================
     if request.method == "POST":
+
+        if owner_id:
+            requiere_permiso(request, 'owners.change_owner')
+        else:
+            requiere_permiso(request, 'owners.add_owner')
 
         name = request.POST.get('name')
         last_name = request.POST.get('last_name')
@@ -71,14 +83,14 @@ def owners_form_edit(request, owner_id=None):
         return redirect('owners')
 
 
-@login_required
+@permisos_requeridos('owners.delete_owner')
 def owners_delete(request, owner_id):
     if request.method == "POST":
         OwnerService.delete_owner(owner_id)
     return redirect('owners')
 
 
-@login_required
+@permisos_requeridos('owners.view_owner')
 def owners_details(request, owner_id):
     owner = OwnerService.get_owner_by_id(owner_id)
     return render(request, 'owners/owners_details.html', {
